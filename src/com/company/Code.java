7b339a26899069b0c5de9831af0c586262cc8751
elonder629 <ericlonder730@gmail.com>
2017-11-02 08:48:32 -0400

    Initial commit

---
+++ src/com/company/Code.java
@@ -0,0 +1,44 @@
+package com.company;
+
+/**
+ * Created by el693 on 11/2/17.
+ */
+public class Code {
+
+        private String myCode;
+        private String temp;
+        private String hiddenCode;
+        public Code(String code)
+        {
+
+            myCode = code;
+            for(int i = 0; i < myCode.length(); i++)
+                temp += "x";
+            hide(0,myCode.length()-1);
+
+        }
+        public String getCode()
+        {
+            return hiddenCode;
+        }
+
+
+        public void hide(int p1, int p2)
+        {
+            if(p1 >= 0 && p1 < p2 && p2 <= myCode.length()-1)
+                hiddenCode = myCode.substring(0, p1 - 1) + myCode.substring(p1, p2-1) + myCode.substring(p2, myCode.length()-1);
+            else if (p1 < 0)
+                System.out.println("p1 must be a postive number");
+            else if (p2 > myCode.length()-1)
+                System.out.println("p2 must be less than the length of Your original code");
+            else if ()
+        }
+
+
+        public void recover(int p1, int p2)
+        {
+            if(p1 >= 0 && p1 < p2 && p2 <= myCode.length()-1){
+                hiddenCode = hiddenCode.substring(0, p1 - 1) + myCode.substring(p1, p2-1)+ myCode.substring(p2, myCode.length());
+            }
+        }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p1;
+        int p2;
+        String tempCode;
+        System.out.println("Please input a string for encryption");
+        tempCode = input.nextLine();
+        Code code = new Code();
+    }
+}
